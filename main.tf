f2acc5a0d7456ecac0ec11fd2de3300342d17919
Mustafa Taylan Ulun <taylan.ulun@outlook.com>
2021-11-11 19:20:26 +0300

    tfstate

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfstate-test123"
-    key            = "tfstate-test123/terraform-state/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
     dynamodb_table = "dynamo_for_tf_lock"
